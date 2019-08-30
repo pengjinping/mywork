e68a1a652aac6e513f18b0d2b14a55b91648189d
Pengjinping <pengjinping@kuainiugroup.com>
2019-08-30 12:52:22 +0800

    减少展示项目

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -22,14 +22,14 @@
                 //{field: 'id', title: 'ID', width: 80, sort: true},
                 {field: 'name', title: '名称', width: 110, totalRowText: '合计'},
                 {field: 'capital', title: '本金', sort: true, totalRow: true},
-                {field: 'balance', title: '余额', totalRow: true},
-                {field: 'market', title: '市值', sort: true, totalRow: true},
+                //{field: 'balance', title: '余额', totalRow: true},
+                //{field: 'market', title: '市值', sort: true, totalRow: true},
                 {field: 'market_balance', title: '总资产', sort: true, totalRow: true},
                 {field: 'profit', title: '盈亏', sort: true, totalRow: true},
                 {field: 'rate', title: '盈亏率(%)'},
                 {field: 'today', title: '日盈亏', sort: true, totalRow: true},
                 {field: 'week', title: '周盈亏', sort: true, totalRow: true},
-                {field: 'month', title: '月盈亏', sort: true, totalRow: true},
+                //{field: 'month', title: '月盈亏', sort: true, totalRow: true},
                 {
                     field: 'id', title: '操作', templet: function (res) {
                         return '<a href="/product/' + res.id + '">明细</a> | ' +
